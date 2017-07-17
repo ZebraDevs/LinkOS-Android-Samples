0f0c1cd2eaa0bb45e3d5c2a0bbcacb0701902ede
Corn <SCorn@zebra.com>
2017-07-17 11:31:17 -0500

    Initial ConnectivityDemo project commit

---
+++ ConnectivityDemo/app/src/main/java/com/zebra/connectivitydemo/MainActivity.java
@@ -0,0 +1,329 @@
+/**
+ * ********************************************
+ * CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published,
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * <p/>
+ * Copyright ZIH Corp. 2015
+ * <p/>
+ * ALL RIGHTS RESERVED
+ * *********************************************
+ */
+
+
+package com.zebra.connectivitydemo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.zebra.sdk.comm.BluetoothConnection;
+import com.zebra.sdk.comm.Connection;
+import com.zebra.sdk.comm.ConnectionException;
+import com.zebra.sdk.comm.TcpConnection;
+import com.zebra.sdk.printer.PrinterLanguage;
+import com.zebra.sdk.printer.PrinterStatus;
+import com.zebra.sdk.printer.SGD;
+import com.zebra.sdk.printer.ZebraPrinter;
+import com.zebra.sdk.printer.ZebraPrinterFactory;
+import com.zebra.sdk.printer.ZebraPrinterLanguageUnknownException;
+import com.zebra.sdk.printer.ZebraPrinterLinkOs;
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Connection connection;
+
+    private RadioButton btRadioButton;
+    private EditText macAddressEditText;
+    private EditText ipAddressEditText;
+    private EditText portNumberEditText;
+    private static final String bluetoothAddressKey = "ZEBRA_DEMO_BLUETOOTH_ADDRESS";
+    private static final String tcpAddressKey = "ZEBRA_DEMO_TCP_ADDRESS";
+    private static final String tcpPortKey = "ZEBRA_DEMO_TCP_PORT";
+    private static final String PREFS_NAME = "OurSavedAddress";
+
+    private Button testButton;
+    private ZebraPrinter printer;
+    private TextView statusField;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        ipAddressEditText = (EditText) this.findViewById(R.id.ipAddressInput);
+        String ip = settings.getString(tcpAddressKey, "");
+        ipAddressEditText.setText(ip);
+
+        portNumberEditText = (EditText) this.findViewById(R.id.portInput);
+        String port = settings.getString(tcpPortKey, "");
+        portNumberEditText.setText(port);
+
+        macAddressEditText = (EditText) this.findViewById(R.id.macInput);
+        String mac = settings.getString(bluetoothAddressKey, "");
+        macAddressEditText.setText(mac);
+
+        TextView t2 = (TextView) findViewById(R.id.launchpad_link);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        statusField = (TextView) this.findViewById(R.id.statusText);
+
+
+        btRadioButton = (RadioButton) this.findViewById(R.id.bluetoothRadio);
+
+
+        RadioGroup radioGroup = (RadioGroup) this.findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.bluetoothRadio) {
+                    toggleEditField(macAddressEditText, true);
+                    toggleEditField(portNumberEditText, false);
+                    toggleEditField(ipAddressEditText, false);
+                } else {
+                    toggleEditField(portNumberEditText, true);
+                    toggleEditField(ipAddressEditText, true);
+                    toggleEditField(macAddressEditText, false);
+                }
+            }
+        });
+        testButton = (Button) this.findViewById(R.id.testButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        enableTestButton(false);
+                        Looper.prepare();
+                        doConnectionTest();
+                        Looper.loop();
+                        Looper.myLooper().quit();
+                    }
+                }).start();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    public ZebraPrinter connect() {
+        setStatus("Connecting...", Color.YELLOW);
+        connection = null;
+        if (isBluetoothSelected()) {
+            connection = new BluetoothConnection(getMacAddressFieldText());
+            SettingsHelper.saveBluetoothAddress(this, getMacAddressFieldText());
+        } else {
+            try {
+                int port = Integer.parseInt(getTcpPortNumber());
+                connection = new TcpConnection(getTcpAddress(), port);
+                SettingsHelper.saveIp(this, getTcpAddress());
+                SettingsHelper.savePort(this, getTcpPortNumber());
+            } catch (NumberFormatException e) {
+                setStatus("Port Number Is Invalid", Color.RED);
+                return null;
+            }
+        }
+
+        try {
+            connection.open();
+            setStatus("Connected", Color.GREEN);
+        } catch (ConnectionException e) {
+            setStatus("Comm Error! Disconnecting", Color.RED);
+            DemoSleeper.sleep(1000);
+            disconnect();
+        }
+
+        ZebraPrinter printer = null;
+
+        if (connection.isConnected()) {
+            try {
+
+                printer = ZebraPrinterFactory.getInstance(connection);
+                setStatus("Determining Printer Language", Color.YELLOW);
+                String pl = SGD.GET("device.languages", connection);
+                setStatus("Printer Language " + pl, Color.BLUE);
+            } catch (ConnectionException e) {
+                setStatus("Unknown Printer Language", Color.RED);
+                printer = null;
+                DemoSleeper.sleep(1000);
+                disconnect();
+            } catch (ZebraPrinterLanguageUnknownException e) {
+                setStatus("Unknown Printer Language", Color.RED);
+                printer = null;
+                DemoSleeper.sleep(1000);
+                disconnect();
+            }
+        }
+
+        return printer;
+    }
+
+    public void disconnect() {
+        try {
+            setStatus("Disconnecting", Color.RED);
+            if (connection != null) {
+                connection.close();
+            }
+            setStatus("Not Connected", Color.RED);
+        } catch (ConnectionException e) {
+            setStatus("COMM Error! Disconnected", Color.RED);
+        } finally {
+            enableTestButton(true);
+        }
+    }
+
+    private void setStatus(final String statusMessage, final int color) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                statusField.setBackgroundColor(color);
+                statusField.setText(statusMessage);
+            }
+        });
+        DemoSleeper.sleep(1000);
+    }
+
+
+    private void sendTestLabel() {
+        try {
+            ZebraPrinterLinkOs linkOsPrinter = ZebraPrinterFactory.createLinkOsPrinter(printer);
+
+            PrinterStatus printerStatus = (linkOsPrinter != null) ? linkOsPrinter.getCurrentStatus() : printer.getCurrentStatus();
+
+            if (printerStatus.isReadyToPrint) {
+                byte[] configLabel = getConfigLabel();
+                connection.write(configLabel);
+                setStatus("Sending Data", Color.BLUE);
+            } else if (printerStatus.isHeadOpen) {
+                setStatus("Printer Head Open", Color.RED);
+            } else if (printerStatus.isPaused) {
+                setStatus("Printer is Paused", Color.RED);
+            } else if (printerStatus.isPaperOut) {
+                setStatus("Printer Media Out", Color.RED);
+            }
+            DemoSleeper.sleep(1500);
+            if (connection instanceof BluetoothConnection) {
+                String friendlyName = ((BluetoothConnection) connection).getFriendlyName();
+                setStatus(friendlyName, Color.MAGENTA);
+                DemoSleeper.sleep(500);
+            }
+        } catch (ConnectionException e) {
+            setStatus(e.getMessage(), Color.RED);
+        } finally {
+            disconnect();
+        }
+    }
+
+    private void enableTestButton(final boolean enabled) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                testButton.setEnabled(enabled);
+            }
+        });
+    }
+
+    /*
+   * Returns the command for a test label depending on the printer control language
+   * The test label is a box with the word "TEST" inside of it
+   *
+   * _________________________
+   * |                       |
+   * |                       |
+   * |        TEST           |
+   * |                       |
+   * |                       |
+   * |_______________________|
+   *
+   *
+   */
+    private byte[] getConfigLabel() {
+        byte[] configLabel = null;
+        try {
+            PrinterLanguage printerLanguage = printer.getPrinterControlLanguage();
+            SGD.SET("device.languages", "zpl", connection);
+
+            if (printerLanguage == PrinterLanguage.ZPL) {
+                configLabel = "^XA^FO17,16^GB379,371,8^FS^FT65,255^A0N,135,134^FDTEST^FS^XZ".getBytes();
+            } else if (printerLanguage == PrinterLanguage.CPCL) {
+                String cpclConfigLabel = "! 0 200 200 406 1\r\n" + "ON-FEED IGNORE\r\n" + "BOX 20 20 380 380 8\r\n" + "T 0 6 137 177 TEST\r\n" + "PRINT\r\n";
+                configLabel = cpclConfigLabel.getBytes();
+            }
+        } catch (ConnectionException e) {
+
+        }
+        return configLabel;
+    }
+
+
+
+    private void doConnectionTest() {
+        printer = connect();
+
+        if (printer != null) {
+            sendTestLabel();
+        } else {
+            disconnect();
+        }
+    }
+
+    private void toggleEditField(EditText editText, boolean set) {
+        /*
+         * Note: Disabled EditText fields may still get focus by some other means, and allow text input.
+         *       See http://code.google.com/p/android/issues/detail?id=2771
+         */
+        editText.setEnabled(set);
+        editText.setFocusable(set);
+        editText.setFocusableInTouchMode(set);
+    }
+
+    private boolean isBluetoothSelected() {
+        return btRadioButton.isChecked();
+    }
+
+    private String getMacAddressFieldText() {
+        return macAddressEditText.getText().toString();
+    }
+
+    private String getTcpAddress() {
+        return ipAddressEditText.getText().toString();
+    }
+
+    private String getTcpPortNumber() {
+        return portNumberEditText.getText().toString();
+    }
+
+
+}
---
+++ ConnectivityDemo/app/src/main/java/com/zebra/connectivitydemo/UIHelper.java
@@ -0,0 +1,97 @@
+/**
+ * *******************************************
+ * CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published,
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * <p/>
+ * Copyright ZIH Corp. 2010
+ * <p/>
+ * ALL RIGHTS RESERVED
+ * *********************************************
+ */
+package com.zebra.connectivitydemo;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.widget.TextView;
+
+/**
+ * @author mlim3 This class handles the display of dialogs.
+ */
+public class UIHelper {
+    ProgressDialog loadingDialog;
+    Activity activity;
+
+    public UIHelper(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void showLoadingDialog(final String message) {
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                public void run() {
+                    loadingDialog = new ProgressDialog(activity,R.style.ButtonAppearance);
+                    loadingDialog.setMessage(message);
+                    loadingDialog.show();
+                    TextView tv1 = (TextView) loadingDialog.findViewById(android.R.id.message);
+                    tv1.setTextAppearance(activity,R.style.ButtonAppearance);
+
+                }
+            });
+        }
+    }
+
+    public void updateLoadingDialog(final String message) {
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                public void run() {
+                    loadingDialog.setMessage(message);
+                }
+            });
+        }
+    }
+
+    public boolean isDialogActive() {
+        if (loadingDialog != null) {
+            return loadingDialog.isShowing();
+        } else {
+            return false;
+        }
+    }
+
+    public void dismissLoadingDialog() {
+        if (activity != null && loadingDialog != null) {
+            loadingDialog.dismiss();
+        }
+    }
+
+    public void showErrorDialog(String errorMessage) {
+        new AlertDialog.Builder(activity).setMessage(errorMessage).setTitle("Error").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        }).create().show();
+    }
+
+    public void showErrorDialogOnGuiThread(final String errorMessage) {
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                public void run() {
+                    new AlertDialog.Builder(activity).setMessage(errorMessage).setTitle("Error").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                            dismissLoadingDialog();
+                        }
+                    }).create().show();
+                }
+            });
+        }
+    }
+
+}
