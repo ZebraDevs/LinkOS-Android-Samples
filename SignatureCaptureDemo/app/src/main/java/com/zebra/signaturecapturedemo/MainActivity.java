01e24ed5b6f8ab7ce38585d6a1c4eab4f9f5aec3
Steven Si <ssi1@zebra.com>
2022-07-25 09:49:54 -0500

    Upgraded to Gradle v4.2.2. Added ZSDK library files.

--- SignatureCaptureDemo/app/src/main/java/com/zebra/signaturecapturedemo/MainActivity.java
+++ SignatureCaptureDemo/app/src/main/java/com/zebra/signaturecapturedemo/MainActivity.java
@@ -1,3 +1,18 @@
+/**
+ * *******************************************
+ * CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published,
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * <p/>
+ * Copyright ZIH Corp. 2010 - 2022
+ * <p/>
+ * ALL RIGHTS RESERVED
+ * *********************************************
+ */
 package com.zebra.signaturecapturedemo;
 
 import android.app.AlertDialog;
